b080c703c271d58fe43fef74bfb830570048a4f4
Ashrith Sagar <ashrithy@iisc.ac.in>
2025-04-11 05:42:18 +0530

    tpgp alignment

--- tpgp/utils/alignment.py
+++ tpgp/utils/alignment.py
@@ -6,6 +6,7 @@ Alignment of demonstations
 import math
 from typing import List, Optional, Tuple, Union
 
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
 from shapely import LineString, Point, shortest_line
@@ -63,3 +64,19 @@ def compute_B(ls: List[LineString], A: Optional[NDArray] = None) -> NDArray:
         for j in range(N):
             B[i, j] = A[i, j] / len(ls[i].coords)
     return B
+
+
+def plot_index_points(
+    ls: List[LineString], A: Optional[NDArray] = None, indices: List[int] = None
+) -> None:
+    """Plot the closest points using A(i, j) for a given indices of trajectories."""
+    A = compute_A(ls) if A is None else A
+    indices = indices or range(len(ls))
+    _, ax = plt.subplots()
+    for i in indices:
+        ax.plot(*ls[i].xy, label=str(i))
+        for j in indices:
+            if i != j:
+                ax.plot(*ls[i].coords[int(A[i, j])], "ro")
+    plt.tight_layout()
+    plt.show()
