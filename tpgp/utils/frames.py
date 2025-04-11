6d466b05b011060ba6decdcf234833d625a0868d
Ashrith Sagar <ashrithy@iisc.ac.in>
2025-04-11 07:06:56 +0530

    tpgp frames

--- tpgp/utils/frames.py
+++ tpgp/utils/frames.py
@@ -3,8 +3,12 @@ utils/frames.py \n
 Frames and transformations
 """
 
+from typing import List
+
 import numpy as np
 from numpy.typing import NDArray
+from scipy.spatial.transform import Rotation
+from shapely import LineString
 
 
 class Frame:
@@ -13,11 +17,15 @@ class Frame:
     """
 
     def __init__(
-        self, index: int, rotation: NDArray = None, translation: NDArray = None
+        self, index: int, rotation: Rotation = None, translation: NDArray = None
     ):
         self.index = index
-        self.rotation = rotation if rotation is not None else np.eye(2)
-        self.translation = translation if translation is not None else np.zeros(2)
+        self.rotation = rotation if rotation is not None else Rotation.identity(2)
+        self.translation = (
+            np.array(translation, dtype=float)
+            if translation is not None
+            else np.zeros(2)
+        )
 
     def __repr__(self):
         return f"Frame(index={self.index})"
@@ -26,28 +34,18 @@ class Frame:
 GlobalFrame = Frame(0)
 
 
-class Demonstration:
-    """
-    A demonstration is a sequence of Cartesian points and a progress value.
-    The progress value is the normalised time for a trajectory; a value between 0 and 1.
-    """
-
-    def __init__(self, points: NDArray, progress: float):
-        self.points = points
-        self.progress = progress
-
-    def __repr__(self):
-        return f"Demonstration(points={self.points}, progress={self.progress})"
+# A demonstration is a sequence of Cartesian points and a progress value.
+# The progress value is the normalised time for a trajectory; a value between 0 and 1.
 
 
-def transform_demonstration_set(D: NDArray, frame: Frame) -> NDArray:
+def transform_demonstration_set(ls: List[LineString], frame: Frame) -> List[LineString]:
     """
     Transform a demonstration set from global frame to a new frame, without affecting the progress values.
     """
-    R, t = frame.rotation, frame.translation
-    H = np.eye(R.shape[0] + 1, dtype=R.dtype)
-    H[:-1, :-1] = R
-    T = np.zeros(t.shape[0] + 1, dtype=t.dtype)
-    T[:-1] = t
-    D_m = T + H @ D
-    return D_m
+    transformed_ls = []
+    R, t = frame.rotation.as_matrix()[:2, :2], frame.translation
+    for demo in ls:
+        points = np.array(demo.coords)
+        transformed_points = points @ R.T + t
+        transformed_ls.append(LineString(transformed_points))
+    return transformed_ls
